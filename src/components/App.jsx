607fe4dde2f0be34da4ced6e400335c5fe5e1861
sami855-ux <samitale56@outlook.com>
2024-08-23 14:29:18 +0500

    Refactoring for light-dark mode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,24 @@ import Features from "./Featuers"
 import WorkFlow from "./WorkFlow"
 import Pricing from "./Pricing"
 import Testmonial from "./Testmonial"
+import Footer from "./Footer"
+import { useState } from "react"
 
 export default function App() {
+  const [theme, setTheme] = useState(true)
+
+  function handelTheme() {
+    setTheme((curr) => !curr)
+  }
   return (
-    <>
-      <Header />
+    <div className="w-screen h-fit" id={theme ? "dark" : "light"}>
+      <Header onTheme={handelTheme} theme={theme} />
       <Home />
       <Features />
       <WorkFlow />
       <Pricing />
       <Testmonial />
-    </>
+      <Footer />
+    </div>
   )
 }
